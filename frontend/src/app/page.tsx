6831c9881db0809a047575ce4aee2004b81fd8c6
arjaybudzz <archessonan.1@gmail.com>
2023-12-28 15:54:59 +0800

    modified homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,13 +2,8 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <main>
-      
-=======
     <main className='w-screen h-screen'>
       <Link href="/login">Go to login</Link>
->>>>>>> generate-login-page
     </main>
   )
 }
