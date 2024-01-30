4ff0a0807d0a2c8488d786a374e832288a81e2db
arjaybudzz <archessonan.1@gmail.com>
2024-01-30 21:38:30 +0800

    added create post button

--- frontend/src/app/feed/components/PostList.tsx
+++ frontend/src/app/feed/components/PostList.tsx
@@ -8,6 +8,11 @@ const PostList = async() => {
 
   return (
     <div className='flex flex-1 bg-gray-300 flex-col justify-start items-center p-4 border-2 h-auto absolute top-14 left-1/3 right-1/3 border-black gap-8'>
+      <div className='flex flex-col w-full h-auto bg-white rounded-xl'>
+        <div className='flex w-full h-14'>
+          <button>What's on your mind?</button>
+        </div>
+      </div>
       {
         data.map((element: {[key: string]: any}, index: number) => {
           return <Post key={index} content={element.content} userId={element.user_id} creationDate={element.created_at}/>
