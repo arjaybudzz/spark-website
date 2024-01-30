679dae2516257e3062a0111059e46080de54f4ab
arjaybudzz <archessonan.1@gmail.com>
2024-01-30 21:27:56 +0800

    added profile name and date

--- frontend/src/app/feed/components/Post.tsx
+++ frontend/src/app/feed/components/Post.tsx
@@ -1,16 +1,23 @@
 import getUser from '@/app/api/user/getUser'
 import React from 'react'
+import { IconButton } from '@mui/material';
+import { VerifiedUser } from '@mui/icons-material';
 
 const Post = async(props: {[key: string]: any}) => {
   const user = await getUser(props.userId);
   return (
     <div className='w-full h-auto flex flex-col border-2 bg-white p-2 rounded-xl gap-5'>
         <div className='flex w-full h-[80px] bg-gray-300 rounded-lg flex-row'>
-          <div className='flex flex-col justify-center items-center'>
-              
+          <div className='flex flex-col justify-center items-center w-1/4 h-full'>
+              <IconButton>
+                <VerifiedUser />
+              </IconButton>
           </div>
-          <div>
-              <h1>{user.username}</h1>
+          <div className='flex flex-col w-3/4 h-full bg-white p-3'>
+              <h1 className='text-xl font-bold'>{user.username}</h1>
+              <div>
+                <h1 className='text-sm'>{props.creationDate}</h1>
+              </div>
           </div>
         </div>
         <div className='flex w-full h-auto min-h-28 p-3 border-2 border-gray-300 rounded-lg'>
--- frontend/src/app/feed/components/PostList.tsx
+++ frontend/src/app/feed/components/PostList.tsx
@@ -10,7 +10,7 @@ const PostList = async() => {
     <div className='flex flex-1 bg-gray-300 flex-col justify-start items-center p-4 border-2 h-auto absolute top-14 left-1/3 right-1/3 border-black gap-8'>
       {
         data.map((element: {[key: string]: any}, index: number) => {
-          return <Post key={index} content={element.content} userId={element.user_id}/>
+          return <Post key={index} content={element.content} userId={element.user_id} creationDate={element.created_at}/>
         })
       }
     </div>
